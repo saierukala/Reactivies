9afbc0c94d17a7840cf59552edc44d09479e800e
saierukala <sai.erukala1508@gamil.com>
2023-09-27 11:23:35 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
+});
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3004");
+    });
 });
 
 var app = builder.Build();
@@ -24,11 +30,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope =app.Services.CreateScope();
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 try
@@ -36,12 +44,11 @@ try
     var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
     await Seed.SeedData(context);
-
 }
 catch (Exception ex)
 {
-   var logger = services.GetRequiredService<ILogger<Program>>();
-   logger.LogError(ex, "An error occured during migration");
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
